2815acd5e9dffd4b9b89145ace23cbeeeb6c90d3
hale-lab <info.hale.lab@gmail.com>
2023-01-03 01:42:05 +0700

    redefine scripts.

--- tribunBeam.py
+++ tribunBeam.py
@@ -2,48 +2,52 @@ import rhinoscriptsyntax as rs
 import ghpythonlib as gh
 
 #buat variabel utk input tebal balok.
-beam_depth = rs.VectorScale([0,0,-1], float(_depth))
+beam_depth = rs.VectorScale([0,0,-1], round(_depth, 3))
 
 #join semua surface terpisah dari input geometri tribun.
 join_faces = rs.JoinSurfaces(_tribun3D)
 
 #proyeksikan garis grid pada geometri tribun.
-beam_top = rs.ProjectCurveToSurface(_inputGrid, join_faces, [0,0,1])
+project_crv = rs.ProjectCurveToSurface(_inputGrid, join_faces, [0,0,1])
 
 #explode hasil proyeksi grid menjadi segmentasi terpisah.
-list_of_crvs = rs.ExplodeCurves(beam_top)
+list_of_crvs = rs.ExplodeCurves(project_crv)
+list_of_crvs.pop(0)
 
 #deteksi ukuran step dan path pada input geometri tribun.
-path = float(rs.CurveLength(list_of_crvs[1]))
-step = float(rs.CurveLength(list_of_crvs[3]))
+riser = round(rs.CurveLength(list_of_crvs[-2]), 3)
+step = round(rs.CurveLength(list_of_crvs[-3]), 3)
 
 #generate titik untuk membentuk profil sisi bawah balok.
 connector_pts = []
 
 for crv in list_of_crvs:
-    check_length = float(rs.CurveLength(crv))
-    if check_length >= float(path - 0.01):
-        move_crv = rs.CopyObject(crv, beam_depth)
-        startPt = rs.CurveStartPoint(move_crv)
-        connector_pts.append(startPt)
+    check_length = round(rs.CurveLength(crv), 3)
+    if check_length == step :
+        length = step
+        startPt = rs.CurveStartPoint(crv)
+        endPt = rs.CurveEndPoint(crv)
+        path = rs.VectorScale(rs.VectorCreate(startPt, endPt), (_depth / length))
+        new_Pt = rs.CopyObject(rs.CurveStartPoint(crv), rs.VectorAdd(beam_depth, path))
+        connector_pts.append(new_Pt)
+    elif check_length > step :
+        length = round(rs.CurveLength(crv), 3)
+        startPt = rs.CurveStartPoint(crv)
+        endPt = rs.CurveEndPoint(crv)
+        path1 = rs.VectorScale(rs.VectorCreate(startPt, endPt), (_depth / length))
+        path2 = rs.VectorScale(rs.VectorCreate(startPt, endPt), ((_depth + step) / length))
+        new_Pt1 = rs.CopyObject(rs.CurveStartPoint(crv), rs.VectorAdd(beam_depth, path1))
+        new_Pt2 = rs.CopyObject(rs.CurveEndPoint(crv), rs.VectorAdd(beam_depth, path2))
+        connector_pts.append(new_Pt1)
+        connector_pts.append(new_Pt2)
 
-        endPt = rs.CurveEndPoint(move_crv)
-        resize_path = rs.VectorScale(rs.VectorCreate(startPt, endPt), float(step/path))
-        movePt = rs.CopyObject(endPt, resize_path)
-        connector_pts.append(movePt)
+connector_pts = connector_pts[1 : -1]
+connector_pts.insert(0, rs.CopyObject(rs.CurveStartPoint(list_of_crvs[0]), beam_depth))
+connector_pts.insert(len(connector_pts), rs.CopyObject(rs.CurveEndPoint(list_of_crvs[-1]), beam_depth))
 
-#hilangkan titik terakhir pada list titik connector.
-connector_pts.pop(-1)
-
-#hilangkan riser terakhir pada profil atas
-beam_top = rs.JoinCurves(list_of_crvs[1:])
-
-#gabung semua curve untuk membentuk profil bawah.
-beam_bottom = rs.JoinCurves([rs.AddPolyline(connector_pts, None),rs.CopyObject(list_of_crvs[-1], beam_depth)], True, None)
-
-#buat cap utk menutup profil atas dan bawah menjadi closed curve.
+beam_top = rs.JoinCurves(list_of_crvs)
+beam_bottom = rs.AddPolyline(connector_pts)
 beam_cap1 = rs.AddLine(rs.CurveStartPoint(beam_top), rs.CurveStartPoint(beam_bottom))
 beam_cap2 = rs.AddLine(rs.CurveEndPoint(beam_top), rs.CurveEndPoint(beam_bottom))
 
-#buat planar surface dari profile edge.
 beam = rs.AddPlanarSrf([beam_top, beam_bottom, beam_cap1, beam_cap2])
